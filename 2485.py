4b7e365a874dfbfc48f9590e027f7d552c7ce70a
Masaki <homemasaki@Masakis-MacBook-Pro.local>
2024-03-13 23:25:21 +0900

    todays challenge

---
+++ 2485.py
@@ -0,0 +1,19 @@
+"""
+https://leetcode.com/problems/find-the-pivot-integer/?envType=daily-question&envId=2024-03-13
+"""
+
+def pivotInteger(n: int) -> int:
+    return n
+
+# Test Cases
+n1: int = 8
+n2: int = 1
+n3: int = 4
+
+def main() -> None:
+    print(f"{pivotInteger(n1) = }")
+    print(f"{pivotInteger(n2) = }")
+    print(f"{pivotInteger(n3) = }")
+
+if __name__ == "__main__":
+    main()
