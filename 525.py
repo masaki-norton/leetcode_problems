c9f7775880ab6f337592e362c9b61e5010130d2a
Masaki <homemasaki@Masakis-MacBook-Pro.local>
2024-03-16 13:19:32 +0900

    start 3/16

---
+++ 525.py
@@ -0,0 +1,18 @@
+"""
+https://leetcode.com/problems/contiguous-array/?envType=daily-question&envId=2024-03-16
+"""
+
+def findMaxLength(nums: list[int]) -> int:
+    return nums
+
+# Test Cases
+nums1 = [0,1]
+nums2 = [0,1,0]
+
+def main() -> None:
+    print(f"{findMaxLength(nums1) = }")
+    print(f"{findMaxLength(nums2) = }")
+    return None
+
+if __name__ == "__main__":
+    main()
