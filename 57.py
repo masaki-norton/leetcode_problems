a16759b946b1f0b1dd0ac5339d3b4f271372ac37
Masaki <homemasaki@Masakis-MacBook-Pro.local>
2024-03-17 22:41:21 +0900

    start 57

---
+++ 57.py
@@ -0,0 +1,17 @@
+"""
+https://leetcode.com/problems/insert-interval/description/?envType=daily-question&envId=2024-03-17
+"""
+
+def insert(intervals: list[list[int]], newInterval: list[int]) -> list[list[int]]:
+    return None
+
+# Test Cases
+test1 = None
+test2 = None
+
+def main() -> None:
+    print(f"{insert(test1, test1) = }")
+    print(f"{insert(test2, test2) = }")
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -4,3 +4,4 @@ Progress tracker for leetcode problems.
 2024.3.13: 2485 \
 2024.3.15: 238 \
 2024.3.16: 525 \
+2024.3.17: 57 \
