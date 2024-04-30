6c5ffdd61b8867d2292c75445682b7fd63ca326a
Masaki <homemasaki@Masakis-MacBook-Pro.local>
2024-04-30 14:14:38 +0900

    162 start

---
+++ 162.py
@@ -0,0 +1,18 @@
+"""
+https://leetcode.com/problems/find-peak-element/description/
+
+"""
+
+def findPeakElement(nums: list[int]) -> int:
+    return None
+
+### Test Cases
+nums1 = [1,2,3,1]   # 2
+nums2 = [1,2,1,3,5,6,4] # 5
+
+def main() -> None:
+    findPeakElement(nums1)
+    return None
+
+if __name__ == "__main__":
+    main()
