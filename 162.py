42f25d8033b9ce35ff68f28620e05cc827b0d5e4
Masaki <homemasaki@Masakis-MacBook-Pro.local>
2024-04-30 17:07:41 +0900

    162 complete

--- 162.py
+++ 162.py
@@ -4,14 +4,27 @@ https://leetcode.com/problems/find-peak-element/description/
 """
 
 def findPeakElement(nums: list[int]) -> int:
-    return None
+    l = 0
+    r = len(nums) - 1
+    m = (l + r) // 2
+
+    while l <= r:
+        if m > 0 and nums[m] < nums[m - 1]:
+            r = m - 1
+            m = (l + r) // 2
+        elif m < (len(nums) - 1) and nums[m] < nums[m + 1]:
+            l = m + 1
+            m = (l + r) // 2
+        else:
+            return m
+
 
 ### Test Cases
 nums1 = [1,2,3,1]   # 2
-nums2 = [1,2,1,3,5,6,4] # 5
+nums2 = [1,2,1,3,5,6,4] # 5, 1
 
 def main() -> None:
-    findPeakElement(nums1)
+    print(findPeakElement(nums2))
     return None
 
 if __name__ == "__main__":
