c16fd87762106474f2ce90511b1ccfc9753f29df
Masaki <homemasaki@Masakis-MacBook-Pro.local>
2024-05-10 13:12:52 +0900

    104 complete

--- 104_max_depth_bin_tree.py
+++ 104_max_depth_bin_tree.py
@@ -3,22 +3,54 @@ https://leetcode.com/problems/maximum-depth-of-binary-tree/description/
 """
 
 from toolbox import listToTree, TreeNode
+from collections import deque
 
 def maxDepth(root: TreeNode) -> int:
+    # DFS
     # Base Casae
     if not root:
         return 0
 
     return 1 + (max(maxDepth(root.left), maxDepth(root.right)))
 
-# Test Cases
+def maxDepth2(root: TreeNode) -> int:
+    # BFS
+    if not root:
+        return 0
 
+    level = 0
+    q = deque([root])
+
+    while q:
+        for i in range(len(q)):
+            node = q.popleft()
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+        level += 1
+    return level
+
+def maxDepth3(root: TreeNode) -> int:
+    # recursive DFS
+    stack = [[root, 1]]
+    result = 0
+
+    while stack:
+        node, depth = stack.pop()
+        if node:
+            result = max(result, depth)
+            stack.append([node.left, depth + 1])
+            stack.append([node.right, depth + 1])
+    return result
+
+# Test Cases
 root1 = [3,9,20,None,None,15,7] # 3
 root2 = [1,None,2]  # 2
 
 def main():
-    print(maxDepth(listToTree(root1)))
-    print(maxDepth(listToTree(root2)))
+    print(maxDepth3(listToTree(root1)))
+    print(maxDepth3(listToTree(root2)))
     return None
 
 if __name__ == "__main__":
