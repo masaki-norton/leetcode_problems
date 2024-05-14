eb162f9a7c99243fc6c5c5ecc5133449817f189a
Masaki <homemasaki@Masakis-MacBook-Pro.local>
2024-05-14 11:01:43 +0900

    79 complete

--- 79_word_search.py
+++ 79_word_search.py
@@ -5,12 +5,11 @@ from collections import deque
 
 def dfs(visited, row, col, board, word, letter):
     # this letter is wrong
-    print(row, col, letter, visited)
     if (row < 0 or row >= len(board) or col < 0 or col >= len(board[row])\
         or board[row][col] != word[letter] or [row, col] in visited):
         return False
 
-    # this letter is right, log in visited, and move onto next letter
+    # this letter is correct, log in visited, and move onto next letter
     visited.append([row, col])
     letter += 1
     if len(visited) == len(word):
@@ -25,6 +24,7 @@ def dfs(visited, row, col, board, word, letter):
     if dfs(visited, row, col + 1, board, word, letter): # right
         return True
 
+    visited.pop()
     return False
 
 def exists(board: list[list[str]], word: str) -> bool:
@@ -41,14 +41,13 @@ def exists(board: list[list[str]], word: str) -> bool:
 # Test Cases
 board1 = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]    # True
 board2 = [["A","B","C","E"],["S","F","C","S"],["A","D","E","E"]]    # True
-board3 = [["A","B","C","E"],["S","F","E","S"],["A","D","E","E"]]    # False
+board3 = [["A","B","C","E"],["S","F","E","S"],["A","D","E","E"]]    # True
 
 word1 = "ABCCED"
 word2 = "SEE"
 word3 = "ABCESEEEFS"
 
 def main() -> None:
-
     print(exists(board3, word3))
     return
 
