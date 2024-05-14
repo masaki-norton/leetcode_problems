d381f98e1b6a3550a8ec7d98eaded74b7a4b6260
Masaki <homemasaki@Masakis-MacBook-Pro.local>
2024-05-14 11:23:50 +0900

    94 start

---
+++ 94_bin_tree_inorder_traversal.py
@@ -0,0 +1,21 @@
+"""
+https://leetcode.com/problems/binary-tree-inorder-traversal/description/
+"""
+
+from toolbox import listToTree, TreeNode
+from collections import deque
+
+def inorderTraversal(self, root: [TreeNode]) -> list[int]:
+    return None
+
+# Test Cases
+root1 = [1,None,2,3]    # [1,3,2]
+root2 = []  # []
+root3 = [1] # [1]
+
+def main():
+    print(inorderTraversal(root1))
+    return None
+
+if __name__ == "__main__":
+    main()
