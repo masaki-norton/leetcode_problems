6c24c362e3430cea46d9bab867d2e99ecbd45ac6
Masaki <homemasaki@Masakis-MacBook-Pro.local>
2024-05-29 08:27:58 +0900

    start 347

---
+++ 347_top_k_frequent_elements.py
@@ -0,0 +1,21 @@
+'''
+https://leetcode.com/problems/top-k-frequent-elements/description/
+'''
+
+def topKFrequent(nums: list[int], k: int) -> list[int]:
+
+    return
+
+# test cases
+nums1 = [1,1,1,2,2,3]
+k1 = 2  # [1,2]
+
+nums2 = [1]
+k2 = 1  # [1]
+
+def main():
+    print(topKFrequent(nums1, k1))
+    print(topKFrequent(nums2, k2))
+
+if __name__ == "__main__":
+    main()
