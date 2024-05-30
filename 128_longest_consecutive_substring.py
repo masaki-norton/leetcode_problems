d993eaecd492dc359908a65765c57de6650d960d
Masaki <masaki.norton@gmail.com>
2024-05-30 21:37:26 +0900

    128 started

---
+++ 128_longest_consecutive_substring.py
@@ -0,0 +1,17 @@
+'''
+https://leetcode.com/problems/longest-consecutive-sequence/description/
+'''
+
+def longestConsecutive(nums: list[int]) -> int:
+    return
+
+# test cases
+nums1 = [100,4,200,1,3,2]   # 4
+nums2 = [0,3,7,2,5,8,4,6,0,1]   # 9
+
+def main():
+    print(longestConsecutive(nums1))
+    print(longestConsecutive(nums2))
+
+if __name__ == "__main__":
+    main()
