3bdbbf764f9b3a47d882afa3c8e39bf4473bcd80
Masaki <masaki.norton@gmail.com>
2024-06-02 10:32:24 +0900

    start 15

---
+++ 15_3_sum.py
@@ -0,0 +1,19 @@
+'''
+https://leetcode.com/problems/3sum/description/
+'''
+
+def threeSum(nums: list[int]) -> list[list[int]]:
+    return
+
+# test cases
+nums1 = [-1,0,1,2,-1,-4] # [[-1,-1,2],[-1,0,1]]
+nums2 = [0,1,1]  # []
+nums3 = [0,0,0]  # [[0,0,0]]
+
+def main():
+    print(threeSum(nums1))
+    print(threeSum(nums2))
+    print(threeSum(nums3))
+
+if __name__ == "__main__":
+    main()
