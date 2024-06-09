db73780c1ee1e0bcd3f6dac61f08d840a214336c
Masaki <masaki.norton@gmail.com>
2024-06-09 19:09:35 +0900

    74 complete

--- binary_search/704_binary_search.py
+++ binary_search/704_binary_search.py
@@ -8,14 +8,14 @@ def search(nums: list[int], target:int) -> int:
 
     while l < r:
         if nums[m] == target:
-            return m
-        elif nums[m] <= target:
+            return (l, r, m)
+        elif nums[m] < target:
             l = m + 1
             m = (r + l) // 2
         else:
             r = m - 1
             m = (r + l) // 2
-    return -1
+    return (l, r, m, -1)
 
 # test cases
 nums1 = [-1,0,3,5,9,12]
@@ -24,9 +24,13 @@ target1 = 9 # 4
 nums2 = [-1,0,3,5,9,12]
 target2 = 2 # -1
 
+nums3 = [1, 10, 23]
+target3 = 3
+
 def main():
     print(search(nums1, target1))
     print(search(nums2, target2))
+    print(search(nums3, target3))
     return None
 
 if __name__ == "__main__":
--- binary_search/74_search_2d_matrix.py
+++ binary_search/74_search_2d_matrix.py
@@ -2,8 +2,34 @@
 https://leetcode.com/problems/search-a-2d-matrix/description/
 '''
 
-def searchMatrix(matrix: list[list[int]]) -> bool:
-    return None
+def searchMatrix(matrix: list[list[int]], target: int) -> bool:
+    b, t = 0, len(matrix) - 1
+    m = (t + b) // 2
+
+    while b < t:
+        if matrix[m][0] == target:
+            return True
+        elif matrix[m][-1] < target:
+            b = m + 1
+            m = (t + b) // 2
+        else:
+            t = m
+            m = (t + b) // 2
+
+    inner_list = matrix[m]
+    l, r = 0, len(inner_list) - 1
+    m = (l + r) // 2
+    while l <= r:
+        if inner_list[m] == target:
+            return True
+        elif inner_list[m] < target:
+            l = m + 1
+            m = (l + r ) // 2
+        else:
+            r = m - 1
+            m = (l + r ) // 2
+
+    return False
 
 # test cases
 matrix1 = [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
