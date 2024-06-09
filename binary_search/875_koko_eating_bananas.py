26d223f723e9b570589cb2305a3bc6218dd7f9e9
Masaki <masaki.norton@gmail.com>
2024-06-09 20:03:26 +0900

    875 started

---
+++ binary_search/875_koko_eating_bananas.py
@@ -0,0 +1,24 @@
+'''
+https://leetcode.com/problems/koko-eating-bananas/description/
+'''
+
+def minEatingSpeed(piles: list[int], h: int) -> int:
+    return None
+
+# test cases
+piles1 = [3,6,7,11]
+h1 = 8  # 4
+piles2 = [30,11,23,4,20]
+h2 = 5  # 30
+piles3 = [30,11,23,4,20]
+h3 = 6  # 23
+
+
+def main():
+    print(minEatingSpeed(piles1, h1))
+    print(minEatingSpeed(piles2, h2))
+    print(minEatingSpeed(piles3, h3))
+    return None
+
+if __name__ == "__main__":
+    main()
