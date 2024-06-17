2e6b7ae283f1caa635264694e83479727675bfef
Masaki <masaki.norton@gmail.com>
2024-06-17 21:49:00 +0900

    4 started

---
+++ binary_search/4_median_two_sorted_arrays.py
@@ -0,0 +1,21 @@
+'''
+https://leetcode.com/problems/median-of-two-sorted-arrays/description/
+'''
+
+def findMedianSortedArrays(nums1: list[int], nums2: list[int]) -> int:
+    return None
+
+# test cases
+nums1a = [1,3]
+nums1b = [2]    # 2.00000
+
+nums2a = [1,2]
+nums2b = [3,4]  # 2.5
+
+def main():
+    print(findMedianSortedArrays(nums1a, nums1b))
+    print(findMedianSortedArrays(nums2a, nums2b))
+    return None
+
+if __name__ == "__main__":
+    main()
