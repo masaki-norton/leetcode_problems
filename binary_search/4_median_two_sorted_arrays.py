c69ea0856b3914b9002df37c76b988e2c1cbbe1c
Masaki <masaki.norton@gmail.com>
2024-06-20 22:07:57 +0900

    continuing 4

--- binary_search/4_median_two_sorted_arrays.py
+++ binary_search/4_median_two_sorted_arrays.py
@@ -3,8 +3,14 @@ https://leetcode.com/problems/median-of-two-sorted-arrays/description/
 '''
 
 def findMedianSortedArrays(numsa: list[int], numsb: list[int]) -> int:
+    print(f"{numsa = }")
+    print(f"{numsb = }")
     # helper method for binary search validation
     def isValidPartition(numsa_index, numsb_index):
+        print(f"{numsa[numsa_index] = }")
+        print(f"{numsb[numsb_index + 1] = }")
+        print(f"{numsb[numsb_index] = }")
+        print(f"{numsa[numsa_index + 1] = }")
         return numsa[numsa_index] <= numsb[numsb_index + 1] and \
             numsb[numsb_index] <= numsa[numsa_index + 1]
 
@@ -17,15 +23,24 @@ def findMedianSortedArrays(numsa: list[int], numsb: list[int]) -> int:
             return (numsa[len(numsa)//2] + numsa[len(numsa)//2 - 1]) / 2
 
     left_partition_index = (len(numsa) + len(numsb) - 2) // 2
+    print(f"{left_partition_index = }")
 
-    l, r = 0, len(numsa) - 1
+    l, r = 0, left_partition_index
     m = (l + r) // 2
 
     while l < r:
-        if isValidPartition(m, left_partition_index - m - 2):
-            return "found median index"
-        elif
-
+        print('=====================')
+        print(f"{(l, m, r) = }")
+        if isValidPartition(m, left_partition_index - m - 1):
+            print(f"partition index: {m = }")
+            break
+        elif numsa[m] > numsb[left_partition_index - m - 1]:
+            r = m - 1
+            m = (l + r) // 2
+        else:
+            l = m +
+            m = (l + r) // 2
+    print(f"partition index: {r = }")
     return None
 
 # test cases
@@ -47,13 +62,27 @@ nums4b = [6,7,8,9,11]
 '''
 combined [1,2,3,4,5,6,7,7,8,9,11]
 ouput: 6
+left_partition_index = (6 + 5 - 2) // 2 = 4 -> index where left side ends in
+    combined sorted array.
 '''
 
 
 def main():
-    # print(findMedianSortedArrays(nums1a, nums1b))
-    # print(findMedianSortedArrays(nums2a, nums2b))
+    print('=====================')
+    print('=====================')
+    print('=====================')
+    print(findMedianSortedArrays(nums1a, nums1b))
+    print('=====================')
+    print('=====================')
+    print('=====================')
+    print(findMedianSortedArrays(nums2a, nums2b))
+    print('=====================')
+    print('=====================')
+    print('=====================')
     print(findMedianSortedArrays(nums3a, nums3b))
+    print('=====================')
+    print('=====================')
+    print('=====================')
     print(findMedianSortedArrays(nums4a, nums4b))
     return None
 
